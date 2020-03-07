28bbaa45ac994a7e3b62c4dc8f069c88e5430933
high-moctane <high.moctane@gmail.com>
2020-03-07 15:50:42 +0900

    ただ実行するだけ(｀･ω･´)

--- main.go
+++ main.go
@@ -1,52 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math"
-	"math/rand"
-	"net/http"
-	"os"
 	"os/exec"
-	"strings"
+	"time"
 )
 
-const jackpotProbability = 0.3
-
-var fireProbability = calcFireProbability()
-
 func main() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, firePenguin())
-	out, _ := exec.Command("ls", "-la").Output()
-	fmt.Fprintln(w, string(out))
-	out, _ = exec.Command("whoami").Output()
-	fmt.Fprintln(w, string(out))
-}
-
-func calcFireProbability() float64 {
-	return math.Pow(jackpotProbability, 1.0/8.0)
-}
-
-func genEmoji() string {
-	if rand.Float64() < fireProbability {
-		return "🔥"
+	err := exec.Command("./mocbinary-unibo", "").Run()
+	if err != nil {
+		log.Fatal(err)
 	}
-	return "❄️"
-}
-
-func firePenguin() string {
-	fireSnow := make([]string, 8)
-	for i := 0; i < 8; i++ {
-		fireSnow[i] = genEmoji()
+	for {
+		time.Sleep(time.Hour)
 	}
-
-	return strings.Join(fireSnow[:3], "") + "\n" +
-		fireSnow[3] + "🐧" + fireSnow[4] + "\n" +
-		strings.Join(fireSnow[5:], "")
 }
